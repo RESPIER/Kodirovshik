924d6daff15565711334cfd350857fa82541e373
My PC <amelcakovu@gmail.com>
2024-02-16 22:05:28 +0200

    2.0

--- main.py
+++ main.py
@@ -2,15 +2,24 @@ import random as r
 
 operattion = ["+", "-", "*", "/"]
 random_operattion = r.choice(operattion)
-print(random_operattion)
 
-a = 10
-b = 5
-if random_operattion == "+":
-    print(a + b)
-elif random_operattion == "-":
-    print(a - b)
-elif random_operattion == "*":
-    print(a * b)
-elif random_operattion == "/":
-    print(a / b)
+
+class Random_number:
+    def print_info(self):
+        __first = 10
+        __second_num = 5
+        if random_operattion == "+":
+            third_num = __first + __second_num
+            print(third_num)
+        elif random_operattion == "-":
+            third_num = __first - __second_num
+            print(third_num)
+        elif random_operattion == "*":
+            third_num = __first * __second_num
+            print(third_num)
+        elif random_operattion == "/":
+            third_num = __first / __second_num
+            print(third_num)
+
+RN = Random_number()
+RN.print_info()
